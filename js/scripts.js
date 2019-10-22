8ef1e42ba0ed6bee7d06d1932fb155044581fd81
Willemijn Bresjer <wbbresjer@gmail.com>
2019-10-22 16:00:39 +0000

    Add all starter files

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// 1: Select the element with the ID 'about'. 
+//    Store the element in the variable `about`.
+const about;
+about.style.border = "2px solid firebrick";
+
+// 2: Select all the <h2> elements in the document.
+//    Set the color of the <h2> elements to a different color.
+
+
+// 3: Select all elements with the class '.card'. 
+//    Set their background color to the color of your choice.
+
+
+// 4: Select only the first <ul> in the document.
+//    Assign it to a variable named `ul`.
+
+ul.style.border = "2px solid indigo";
+
+// 5: Select only the second element with the class '.container'.
+//    Assign it to a variable named `container`.
+
+container.style.backgroundColor = "royalblue";
+
+// 6: Select all <a> elements that have a 'title' attribute. 
+//    Set their color value to the color of your choice.
+
+
